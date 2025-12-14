7e3f820d94d9b7cef81e5b75f14a02ada2bc979d
OMDD OM <o.matzner@gmail.com>
2025-12-14 10:19:46 +0100

    changed text

--- scripts/infoData.js
+++ scripts/infoData.js
@@ -62,6 +62,10 @@
 
     "neolithic": "Era...",
 
-    "Set Theory": "Set theory is the mathematical theory of well-determined collections, called sets, of objects that are called members, or elements, of the set. Pure set theory deals exclusively with sets, so the only sets under consideration are those whose members are also sets. The theory of the hereditarily-finite sets, namely those finite sets whose elements are also finite sets, the elements of which are also finite, and so on, is formally equivalent to arithmetic. So, the essence of set theory is the study of infinite sets, and therefore it can be defined as the mathematical theory of the actual—as opposed to potential—infinite."
+    "Set Theory": "Set theory is the mathematical theory of well-determined collections, called sets, of objects that are called members, or elements, of the set. Pure set theory deals exclusively with sets, so the only sets under consideration are those whose members are also sets. The theory of the hereditarily-finite sets, namely those finite sets whose elements are also finite sets, the elements of which are also finite, and so on, is formally equivalent to arithmetic. So, the essence of set theory is the study of infinite sets, and therefore it can be defined as the mathematical theory of the actual—as opposed to potential—infinite.",
+
+    "WikiLeaks": "Text about WikiLeaks",
+
+    "PRISM": "Text about PRISM"
 
   };
